3989af70052902d22be8907b1a1e521f834827e5
Isak Song <thddltkr10@naver.com>
2023-01-06 15:01:08 +0900

    health.sh 오타수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -9,13 +9,13 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> culr -s http://localhost:$IDLE_PORT/profile"
+echo "> curl -s http://localhost:$IDLE_PORT/profile"
 sleep 10
 
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
-  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $ up_count >= 1 ("real" 문자열이 있는지 검증)
@@ -27,7 +27,7 @@ do
     echo "> Health check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10]
+  if [ ${RETRY_COUNT} -eq 10 ]
   then
     echo "> Health check 실패."
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다"
